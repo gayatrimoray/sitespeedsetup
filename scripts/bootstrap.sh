dfbcdac849c61e5a901c8ac90b57e674f8224541
Gayatri Moray <gayatrimoray@gmail.com>
2016-03-05 11:35:33 -0800

    Update bootstrap
    More dependencies & echo for markers

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,14 +3,22 @@
 set -e
 
 sudo apt-get update
+
+# install basic dependencies
+echo "[Install] dependencies"
 sudo apt-get install -y git
+sudo apt-get install -y curl
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04
 # How To Install Using a PPA steps followed
-
+echo "[Install] npm"
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
+# update to latest npm
+echo "[Update] npm@latest-2"
 sudo npm install -g npm@latest-2
+# install sitespeed
+echo "[Install] sitespeed"
 sudo npm install -g sitespeed.io
