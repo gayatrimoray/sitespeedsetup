9f2307880ed32e88cf4663a67cfb717a8538bb05
Gayatri Moray <gayatrimoray@gmail.com>
2016-03-05 15:04:20 -0800

    moar dependencies

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,6 +8,8 @@ sudo apt-get update
 echo "[Install] dependencies"
 sudo apt-get install -y git
 sudo apt-get install -y curl
+sudo apt-get install -y libfontconfig
+sudo apt-get install -y default-jre
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04
 # How To Install Using a PPA steps followed
